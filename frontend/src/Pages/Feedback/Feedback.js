3e03f038c4a6c7e08509aa9b72e23b40d6741747
Shahnwaz <avengershahnwaz1@gmail.com>
2022-04-17 17:00:29 +0530

    Feedback Page Updated

--- frontend/src/Pages/Feedback/Feedback.js
+++ frontend/src/Pages/Feedback/Feedback.js
@@ -1,8 +1,39 @@
 import "./Feedback.css";
-import { MDBInput, MDBTextArea } from "mdb-react-ui-kit";
+import emailjs from 'emailjs-com';
 import LeftNav from "../../components/LeftNav/LeftNav";
 import { Navigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+import {
+  MDBInput,
+  MDBTextArea,
+  MDBModalBody,
+  MDBModalContent,
+  MDBModalDialog,
+  MDBModalFooter,
+  MDBBtn
+} from "mdb-react-ui-kit";
 const Feedback = () => {
+  const sendFeedback = (e)=>{
+      e.preventDefault();
+      emailjs.sendForm('service_d64zsci','template_qnnvkvp',e.target,"NQgpo2o9vpYJX41zY"
+      ).then( 
+      <MDBModalDialog  centered >
+        <MDBModalContent className="square border border-success">
+          <MDBModalBody >
+            <b className="text-success" >Feedback Sent.</b>
+          </MDBModalBody>
+          <MDBModalFooter>
+            <Link to="/">
+              <MDBBtn className="bg-success">Home</MDBBtn>
+            </Link>
+          </MDBModalFooter>
+        </MDBModalContent>
+      </MDBModalDialog>
+      ).catch(err=>{
+        console.log(err);
+      });
+  }
   if(localStorage.getItem("myid")===null){
     return <Navigate to="/login" />;
 }
@@ -17,16 +48,23 @@ const Feedback = () => {
         <div className="Feedback">
           <div className="Feedback_header">
             <h2 className="text-success">Feedback Form </h2>
-            <h4>We value your Feedback</h4>
+            <h4>We value your feedback</h4>
             <hr></hr>
           </div>
-          <form >
+          <form onSubmit={sendFeedback}>
+          <MDBInput
+            label="Name"
+              type="text"
+              placeholder="Name"
+              className="designation"
+              name="Name"
+            ></MDBInput>
             <MDBInput
             label="Email"
               type="email"
               placeholder="Email"
               className="designation"
-              name="email"
+              name="Email"
             ></MDBInput>
             <MDBTextArea
             label="feedback"
